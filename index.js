721ca4cf075b2de1d80045edfb78a6f3767d16db
Keith Murimi <keith.murimi@student.moringaschool.com>
2024-07-22 12:46:28 +0300

    Frst commit

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(Feet){
+  if (Feet <= 400) {
+    return 'This one is on me!';
+  } else if (Feet > 2000 && Feet <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+  } else if (Feet > 2500) {
+    return 'No can do.';
+  } else {
+    return 'That will be twenty bucks.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
 }
